4f9eb66225947d829430f6ab4a7089951ad04830
ELiza <235543@student.pwr.edu.pl>
2020-03-17 15:01:20 +0100

    Welcome screen

---
+++ app/src/main/java/com/example/fridgefriend/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.fridgefriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/fridgefriend/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.example.fridgefriend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private static int TIME_OUT = 1500;
+    private static final String TAG = "WelcomeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.xml.fade_in,R.xml.fade_out);
+                finish();
+            }
+        },TIME_OUT);
+
+    }
+}
