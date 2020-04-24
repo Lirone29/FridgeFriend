d9f4e9fe4ee163837fec4eaaac53afa46cf35aa4
ELiza <235543@student.pwr.edu.pl>
2020-04-24 12:37:16 +0200

    Adding frame of fridge activity

---
+++ app/src/main/java/com/example/fridgefriend/FridgeActivity.java
@@ -0,0 +1,34 @@
+package com.example.fridgefriend;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FridgeActivity extends AppCompatActivity {
+    private static final String TAG = "FridgeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fridge);
+        initView();
+
+
+    }
+
+    private void initView() {
+
+    }
+
+    private void addProduct(){
+
+    }
+
+    private void removeProduct(){
+
+    }
+
+    private void checkExpireDate(){
+
+    }
+}
--- app/src/main/java/com/example/fridgefriend/MainActivity.java
+++ app/src/main/java/com/example/fridgefriend/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.fridgefriend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    Button _fridgeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        _fridgeButton = findViewById(R.id.registerButton);
+
+        _fridgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FridgeActivity.class);
+                startActivityForResult(intent, 1);
+                finish();
+            }
+        });
     }
 
     private void initView() {
